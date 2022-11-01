779337772acd1ddaefad4316c951b9a81e57b6aa
Usama Khan <usamaarshadkhan@gmail.com>
2022-11-01 13:25:51 -0500

    Completed lab 8

--- ticketLog/models.py
+++ ticketLog/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 
+
 class Days(models.TextChoices):
-    Sunday="U"
-    Monday="M"
-    Tuesday="T"
-    Wednesday="W"
-    Thursday="R"
-    Friday="F"
-    Saturday="S"
+    Sunday = "U"
+    Monday = "M"
+    Tuesday = "T"
+    Wednesday = "W"
+    Thursday = "R"
+    Friday = "F"
+    Saturday = "S"
+
 
 class Sections(models.TextChoices):
-    A="A"
-    B="B"
-    C="C"
-    D="D"
-    E="E"
-    F="F"
-    G="G"
+    A = "A"
+    B = "B"
+    C = "C"
+    D = "D"
+    E = "E"
+    F = "F"
+    G = "G"
+
 
 class Ticket(models.Model):
     dateTime = models.DateTimeField()
